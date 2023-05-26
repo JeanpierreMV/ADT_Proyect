a860e9fb9ad260ba78e0f21f0acc425ac229fd36
JeanpierreMV <jeanpierre_montalvo@usmp.pe>
2023-05-26 10:41:58 -0500

    3DSCENE

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,26 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿ // Configura el contenedor y la escena
+ var container = document.getElementById('canvas-container');
+ var scene = new THREE.Scene();
 
-// Write your JavaScript code.
+ // Configura la cámara
+ var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+ camera.position.z = 5;
+
+ // Configura el renderizador
+ var renderer = new THREE.WebGLRenderer();
+ renderer.setSize(window.innerWidth, window.innerHeight);
+ container.appendChild(renderer.domElement);
+
+ // Carga el modelo 3D
+ var loader = new THREE.OBJLoader();
+ loader.load('ruta_al_archivo.obj', function (object) {
+     scene.add(object);
+ });
+
+ // Renderiza la escena
+ function animate() {
+     requestAnimationFrame(animate);
+     renderer.render(scene, camera);
+ }
+ animate();
+
